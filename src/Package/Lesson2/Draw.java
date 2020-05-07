40cbb73636507edc926c25d140410df4cfbb2541
Cat <tanasohlo.yurii@gmail.com>
2020-05-07 09:55:51 +0300

    Lesson 2 v0.4: Draw.envelope update

--- src/Package/Lesson2/Draw.java
+++ src/Package/Lesson2/Draw.java
@@ -34,20 +34,29 @@ public class Draw {
     }
 
     public void envelope() {
-        System.out.print("Please input length of the side of the envelope: ");
+
+        System.out.print("Please input length of the first side of the envelope: ");
         Scanner input = new Scanner(System.in);
-        int side = input.nextInt();
+        double firstSide = input.nextInt();
 
+        System.out.print("Please input length of the second side of the envelope: ");
+        input = new Scanner(System.in);
+        double secondSide = input.nextInt();
 
-        for (int i = 0; i < side; i++) {
+        double slopeSteepness = (firstSide - 1) / (secondSide - 1);
+
+        for (int i = 0; i < secondSide; i++) {
             String line = "";
-            if (i == 0 || i + 1 == side) {
-                for (int j = 0; j < side; j++) {
+            if (i == 0 || i + 1 == secondSide) {
+                for (int j = 0; j < firstSide; j++) {
                     line += "*";
                 }
             } else {
-                for (int j = 0; j < side; j++) {
-                    if (j == 0 || j + 1 == side || i == j || i + 1 == side - j) {
+                int positionFirstDiagonal = (int) Math.round(slopeSteepness * i);
+                int positionSecondDiagonal = (int) Math.round(firstSide - slopeSteepness * i);
+
+                for (int j = 0; j < firstSide; j++) {
+                    if (j == 0 || j + 1 == firstSide || j == positionFirstDiagonal || j + 1 == positionSecondDiagonal) {
                         line += "*";
                     } else {
                         line += " ";
