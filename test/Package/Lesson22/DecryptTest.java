455f32598830cdbd97cdae91b8d825bec5a332b2
Cat <tanasohlo.yurii@gmail.com>
2020-07-26 16:43:38 +0300

    Lesson22 v0.1

---
+++ test/Package/Lesson22/DecryptTest.java
@@ -0,0 +1,13 @@
+package Package.Lesson22;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecryptTest {
+    @Test
+    void getLength() {
+        Decrypt decrypt = new Decrypt();
+        decrypt.getLength();
+    }
+}
